b9f925cc1061df951f75a48c81d51b41f6190e4a
lukeklinker <lklinker1@gmail.com>
2013-06-26 21:41:21 -0500

    added changelog and tweet us button to the about screen, along with my name haha

--- src/com/klinker/android/messaging_sliding/MainActivity.java
+++ src/com/klinker/android/messaging_sliding/MainActivity.java
@@ -3899,6 +3899,8 @@ s
 		
 		return true;
 	}
+
+    public String version;
 	
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
@@ -3921,7 +3923,7 @@ s
 	    case R.id.menu_about:
 	    	AlertDialog.Builder builder = new AlertDialog.Builder(this);
 	    	builder.setTitle(R.string.menu_about);
-	    	String version = "";
+	    	version = "";
 	    	
 	    	try {
 				version = this.getPackageManager().getPackageInfo(this.getPackageName(), 0).versionName;
@@ -3930,7 +3932,22 @@ s
 			}
 
             builder.setMessage(this.getResources().getString(R.string.version) + ": " + version +
-                    "\n\n" + this.getResources().getString(R.string.about_expanded) + "\n\n© 2013 Jacob Klinker");
+                    "\n\n" + this.getResources().getString(R.string.about_expanded) + "\n\n© 2013 Jacob Klinker and Luke Klinker")
+                    .setPositiveButton("Change Log", new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int id) {
+                            Intent wizardintent = new Intent(getApplicationContext(), wizardpager.MainActivity.class);
+                            wizardintent.putExtra("version", version);
+                            startActivity(wizardintent);
+                        }
+                    })
+                    .setNegativeButton("Tweet Us", new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int id) {
+                            Intent sharingIntent = new Intent(Intent.ACTION_SEND);
+                            sharingIntent.setType("text/plain");
+                            sharingIntent.putExtra(android.content.Intent.EXTRA_TEXT, "@sliding_sms ");
+                            startActivity(Intent.createChooser(sharingIntent, "Share using"));
+                        }
+                    });
 			
 			AlertDialog dialog = builder.create();
 			dialog.show();
