2cbda4526c0eac5fc6463e8ecf33915f16fcd5c4
Jake Klinker <jklinker1@gmail.com>
2013-08-05 19:46:31 +0000

    Added hardware accelerated flag, not sure if necessary or not, but can't hurt

--- src/com/klinker/android/messaging_sliding/slide_over/SlideOverService.java
+++ src/com/klinker/android/messaging_sliding/slide_over/SlideOverService.java
@@ -48,8 +48,9 @@ public class SlideOverService extends Service {
                 halo.getHeight(),
                 0,
                 0,
-                WindowManager.LayoutParams.TYPE_PHONE,
+                WindowManager.LayoutParams.TYPE_SYSTEM_ALERT,
                 WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE
+                        |WindowManager.LayoutParams.FLAG_HARDWARE_ACCELERATED
                         |WindowManager.LayoutParams.FLAG_NOT_TOUCH_MODAL
                         |WindowManager.LayoutParams.FLAG_WATCH_OUTSIDE_TOUCH,
                 PixelFormat.TRANSLUCENT);
@@ -93,6 +94,7 @@ public class SlideOverService extends Service {
                             params = new WindowManager.LayoutParams(
                                     WindowManager.LayoutParams.TYPE_PHONE,
                                     WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE
+                                            |WindowManager.LayoutParams.FLAG_HARDWARE_ACCELERATED
                                             |WindowManager.LayoutParams.FLAG_NOT_TOUCH_MODAL
                                             |WindowManager.LayoutParams.FLAG_WATCH_OUTSIDE_TOUCH
                                             |WindowManager.LayoutParams.FLAG_DIM_BEHIND,
@@ -153,8 +155,9 @@ public class SlideOverService extends Service {
                                     halo.getHeight(),
                                     0,
                                     0,
-                                    WindowManager.LayoutParams.TYPE_PHONE,
+                                    WindowManager.LayoutParams.TYPE_SYSTEM_ALERT,
                                     WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE
+                                            |WindowManager.LayoutParams.FLAG_HARDWARE_ACCELERATED
                                             |WindowManager.LayoutParams.FLAG_NOT_TOUCH_MODAL
                                             |WindowManager.LayoutParams.FLAG_WATCH_OUTSIDE_TOUCH,
                                     PixelFormat.TRANSLUCENT);
