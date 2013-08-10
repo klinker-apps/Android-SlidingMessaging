e2f4f71aa6c2371adc51fd66d98d5d1d12a1f40b
Jacob Klinker <jklinker1@gmail.com>
2013-08-10 00:04:33 -0500

    halo settings popup should work fine now...

--- src/com/klinker/android/messaging_sliding/slide_over/SlideOverSettings.java
+++ src/com/klinker/android/messaging_sliding/slide_over/SlideOverSettings.java
@@ -1,25 +1,13 @@
 package com.klinker.android.messaging_sliding.slide_over;
 
-import java.util.Locale;
-
-import android.app.ActionBar;
-import android.appwidget.AppWidgetManager;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.content.res.Configuration;
-import android.content.res.Resources;
-import android.graphics.Point;
-import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.os.Handler;
 import android.preference.Preference;
 import android.preference.PreferenceActivity;
 import android.preference.PreferenceManager;
-import android.view.*;
-import android.widget.Button;
-import android.widget.RemoteViews;
-import android.widget.Toast;
 import com.klinker.android.messaging_donate.R;
 
 public class SlideOverSettings  extends PreferenceActivity {
@@ -34,16 +22,9 @@ public class SlideOverSettings  extends PreferenceActivity {
     public void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
-        
-        requestWindowFeature(Window.FEATURE_NO_TITLE);
 
         addPreferencesFromResource(R.xml.slideover_settings);
         setTitle(R.string.slide_over);
-
-
-        getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_RESIZE);
-
-        setUpWindow();
 
         sharedPrefs = PreferenceManager.getDefaultSharedPreferences(this);
 
@@ -110,27 +91,6 @@ public class SlideOverSettings  extends PreferenceActivity {
         }
     }
 
-    public void setUpWindow()
-    {
-        setTitle(null);
-
-        ColorDrawable background = new ColorDrawable();
-        background.setColor(getResources().getColor(android.R.color.black));
-        getWindow().setBackgroundDrawable(background);
-
-        Display display = getWindowManager().getDefaultDisplay();
-        Point size = new Point();
-        display.getSize(size);
-        int width = size.x;
-        int height = size.y;
-
-        if (width > height) {
-            getWindow().getDecorView().setPadding(50,height/12,50,height/12);
-        } else {
-            getWindow().getDecorView().setPadding(width / 8, height /10 , width / 8, height / 10);
-        }
-    }
-
     public void restartHalo() {
         Intent service = new Intent();
         service.setAction("com.klinker.android.messaging.STOP_HALO");
@@ -145,22 +105,5 @@ public class SlideOverSettings  extends PreferenceActivity {
                 }
             }
         }, 500);
-    }
-
-    @Override
-    public void onConfigurationChanged(Configuration newConfig) {
-        super.onConfigurationChanged(newConfig);
-
-        Display display = getWindowManager().getDefaultDisplay();
-        Point size = new Point();
-        display.getSize(size);
-        int width = size.x;
-        int height = size.y;
-
-        if (width > height) {
-            getWindow().getDecorView().setPadding(50,height/12,50,height/12);
-        } else {
-            getWindow().getDecorView().setPadding(width / 8, height / 10 , width / 8, height / 10);
-        }
     }
 }
