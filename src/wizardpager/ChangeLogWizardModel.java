72bc89498e4e29c63251bb51f49f742dffa7a219
Jacob Klinker <jklinker1@gmail.com>
2013-08-24 15:16:15 -0500

    added link to github library in changelog

--- src/wizardpager/ChangeLogWizardModel.java
+++ src/wizardpager/ChangeLogWizardModel.java
@@ -59,7 +59,7 @@ public class ChangeLogWizardModel extends AbstractWizardModel {
                 "- For interested developers: I completely recreated the back-end sending process and published t it as a library to Github. Any developer interested in using it, improving it, or whatever else, check it out! " +
                 "There is support for sending SMS and MMS messages all packaged into one easy class to use - this is something Google has neglected for much too long, so with this anyone can easily send " +
                 "any type of message from within their application. Think of it as the missing MMS APIs if you want and save yourself the trouble of countless hours digging through their source code trying to figure out what is going on." +
-                " <INSERT LINK TO GITHUB PROJECT HERE>";
+                " https://github.com/klinker41/android-smsmms";
                 
         String themeEditor = "The theme editor now fully supports the Cards UI 2.0, making it a " +
                 "better time than ever to get on board and start making Sliding Messaging Pro look exactly how you want!\n\n" +
