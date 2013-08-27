538b97ac90875f7d115f859a3d5bce3a20e83daf
Jacob Klinker <jklinker1@gmail.com>
2013-08-27 12:25:33 -0500

    new beta coming up

--- src/wizardpager/ChangeLogWizardModel.java
+++ src/wizardpager/ChangeLogWizardModel.java
@@ -46,7 +46,8 @@ public class ChangeLogWizardModel extends AbstractWizardModel {
         }
 
         String changeLog = "Version " + version + ":\n\n" +
-                "- Bug fixes\n\n" +
+                "- Select all for batch delete\n" +
+                "- Lots of bug fixes\n\n" +
                 "- For interested developers: I completely recreated the back-end sending process and published it as a library to Github. Any developer interested in using it, improving it, or whatever else, check it out! " +
                 "There is support for sending SMS and MMS messages all packaged into one easy class to use - this is something Google has neglected for much too long, so with this anyone can easily send " +
                 "any type of message from within their application. Think of it as the missing MMS APIs if you want and save yourself the trouble of countless hours digging through their source code trying to figure out what is going on." +
