df799ad0e42b4d61d0e77d13be0e520d4e38edf0
Jacob Klinker <jklinker1@gmail.com>
2013-10-02 13:14:44 -0500

    updated translation

--- src/com/klinker/android/messaging_sliding/receivers/VoicePushReceiver.java
+++ src/com/klinker/android/messaging_sliding/receivers/VoicePushReceiver.java
@@ -3,11 +3,13 @@ package com.klinker.android.messaging_sliding.receivers;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.util.Log;
 
 public class VoicePushReceiver extends BroadcastReceiver {
 
     @Override
     public void onReceive(Context context, Intent intent) {
+        Log.v("voice_push", "voice message received, starting download");
         context.startService(new Intent(context, VoiceReceiver.class));
     }
 }
