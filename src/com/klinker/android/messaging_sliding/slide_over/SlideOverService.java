6338bf7a4ebf1fa46521942b3311897897bd0b85
Jake Klinker <jklinker1@gmail.com>
2013-12-17 15:15:22 -0600

    fix one force close

--- src/com/klinker/android/messaging_sliding/slide_over/SlideOverService.java
+++ src/com/klinker/android/messaging_sliding/slide_over/SlideOverService.java
@@ -223,7 +223,7 @@ public class SlideOverService extends Service {
                 if (Build.VERSION.SDK_INT > Build.VERSION_CODES.ICE_CREAM_SANDWICH) {
                     notification.priority = Notification.PRIORITY_MIN;
                 }
-            } catch (Exception e) {
+            } catch (Throwable e) {
 
             }
 
