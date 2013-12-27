5a3722edd08666c7f22112c8a4afa16e68bbf4c4
lukeklinker <lklinker1@gmail.com>
2013-12-26 20:05:06 -0600

    changes to the touch zone to reflect the new drawing size

--- src/com/klinker/android/messaging_sliding/slide_over/SlideOverService.java
+++ src/com/klinker/android/messaging_sliding/slide_over/SlideOverService.java
@@ -890,7 +890,7 @@ public class SlideOverService extends Service {
         float rawY = event.getRawY();
         float rawX = event.getRawX();
 
-        if ((rawY < 120 && PERCENT_DOWN_SCREEN > height / 2) || (rawY > height - 120 && PERCENT_DOWN_SCREEN < height / 2)) // in Button Area
+        if ((rawY < 150 && PERCENT_DOWN_SCREEN > height / 2) || (rawY > height - 150 && PERCENT_DOWN_SCREEN < height / 2)) // in Button Area
         {
             inButtons = true;
 
@@ -1009,7 +1009,7 @@ public class SlideOverService extends Service {
         float rawY = event.getRawY();
         float rawX = event.getRawX();
 
-        if ((rawY < 120 && PERCENT_DOWN_SCREEN > height / 2) || (rawY > height - 120 && PERCENT_DOWN_SCREEN < height / 2)) // in Button Area
+        if ((rawY < 150 && PERCENT_DOWN_SCREEN > height / 2) || (rawY > height - 150 && PERCENT_DOWN_SCREEN < height / 2)) // in Button Area
         {
             inButtons = true;
 
